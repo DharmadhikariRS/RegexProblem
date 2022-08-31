2d9acf61a7241c8d201805ae93e06ed49f647eb3
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-08-31 23:15:20 +0530

    [Rushikesh] firstname with first letter caps

---
+++ RegexProblems/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace RegexProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            RegexProblem regexProblem = new RegexProblem();
+            //UC1
+            regexProblem.CheckFirstName("Rushi");
+        }
+    }
+}
